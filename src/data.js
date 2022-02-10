f0f7ec22138b471afbdfeefb1fed41709ddf3b3a
Batuhan TURGUT <batuhankutahya43@gmail.com>
2022-02-10 17:24:30 +0300

    .

---
+++ src/data.js
@@ -0,0 +1,746 @@
+import room1 from "./images/details-1.jpeg";
+import img1 from "./images/room-1.jpeg";
+
+export default [
+  {
+    sys: {
+      id: "1"
+    },
+    fields: {
+      name: "single economy",
+      slug: "single-economy",
+      type: "single",
+      price: 100,
+      size: 200,
+      capacity: 1,
+      pets: false,
+      breakfast: false,
+      featured: false,
+      description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed laboriosam praesentium incidunt voluptatum voluptas maxime soluta rerum vitae amet quasi, ipsum cupiditate voluptatibus perferendis corrupti ullam facilis sit aliquid porro.",
+      extras: [
+        "Lorem ipsum dolor sit amet consectetur",
+        "Nam fermentum magna ac tortor auctor, semper laoreet t",
+        "Ut fringilla placerat sapien sed rhoncu",
+        "Vestibulum eu enim vel dolor ornare semper quis ornare ",
+        "Aliquam pharetra vulputate laoreet",
+        "Nulla sit amet tempor libero",
+        "Sed condimentum in odio in congue"
+      ],
+      images: [
+        {
+          fields: {
+            file: {
+              url: img1
+            }
+          }
+        },
+        {
+          fields: {
+            file: {
+              url: room1
+            }
+          }
+        },
+        {
+          fields: {
+            file: {
+              url: room1
+            }
+          }
+        },
+        {
+          fields: {
+            file: {
+              url: room1
+            }
+          }
+        }
+      ]
+    }
+  },
+  {
+    sys: {
+      id: "2"
+    },
+    fields: {
+      name: "single economy",
+      slug: "single-economy",
+      type: "single",
+      price: 100,
+      size: 200,
+      capacity: 1,
+      pets: false,
+      breakfast: false,
+      featured: false,
+      description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed laboriosam praesentium incidunt voluptatum voluptas maxime soluta rerum vitae amet quasi, ipsum cupiditate voluptatibus perferendis corrupti ullam facilis sit aliquid porro.",
+      extras: [
+        "Lorem ipsum dolor sit amet consectetur",
+        "Nam fermentum magna ac tortor auctor, semper laoreet t",
+        "Ut fringilla placerat sapien sed rhoncu",
+        "Vestibulum eu enim vel dolor ornare semper quis ornare ",
+        "Aliquam pharetra vulputate laoreet",
+        "Nulla sit amet tempor libero",
+        "Sed condimentum in odio in congue"
+      ],
+      images: [
+        {
+          fields: {
+            file: {
+              url: img1
+            }
+          }
+        },
+        {
+          fields: {
+            file: {
+              url: room1
+            }
+          }
+        },
+        {
+          fields: {
+            file: {
+              url: room1
+            }
+          }
+        },
+        {
+          fields: {
+            file: {
+              url: room1
+            }
+          }
+        }
+      ]
+    }
+  },
+  {
+    sys: {
+      id: "3"
+    },
+    fields: {
+      name: "single economy",
+      slug: "single-economy",
+      type: "single",
+      price: 100,
+      size: 200,
+      capacity: 1,
+      pets: false,
+      breakfast: false,
+      featured: false,
+      description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed laboriosam praesentium incidunt voluptatum voluptas maxime soluta rerum vitae amet quasi, ipsum cupiditate voluptatibus perferendis corrupti ullam facilis sit aliquid porro.",
+      extras: [
+        "Lorem ipsum dolor sit amet consectetur",
+        "Nam fermentum magna ac tortor auctor, semper laoreet t",
+        "Ut fringilla placerat sapien sed rhoncu",
+        "Vestibulum eu enim vel dolor ornare semper quis ornare ",
+        "Aliquam pharetra vulputate laoreet",
+        "Nulla sit amet tempor libero",
+        "Sed condimentum in odio in congue"
+      ],
+      images: [
+        {
+          fields: {
+            file: {
+              url: img1
+            }
+          }
+        },
+        {
+          fields: {
+            file: {
+              url: room1
+            }
+          }
+        },
+        {
+          fields: {
+            file: {
+              url: room1
+            }
+          }
+        },
+        {
+          fields: {
+            file: {
+              url: room1
+            }
+          }
+        }
+      ]
+    }
+  },
+  {
+    sys: {
+      id: "4"
+    },
+    fields: {
+      name: "single economy",
+      slug: "single-economy",
+      type: "single",
+      price: 100,
+      size: 200,
+      capacity: 1,
+      pets: false,
+      breakfast: false,
+      featured: false,
+      description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed laboriosam praesentium incidunt voluptatum voluptas maxime soluta rerum vitae amet quasi, ipsum cupiditate voluptatibus perferendis corrupti ullam facilis sit aliquid porro.",
+      extras: [
+        "Lorem ipsum dolor sit amet consectetur",
+        "Nam fermentum magna ac tortor auctor, semper laoreet t",
+        "Ut fringilla placerat sapien sed rhoncu",
+        "Vestibulum eu enim vel dolor ornare semper quis ornare ",
+        "Aliquam pharetra vulputate laoreet",
+        "Nulla sit amet tempor libero",
+        "Sed condimentum in odio in congue"
+      ],
+      images: [
+        {
+          fields: {
+            file: {
+              url: img1
+            }
+          }
+        },
+        {
+          fields: {
+            file: {
+              url: room1
+            }
+          }
+        },
+        {
+          fields: {
+            file: {
+              url: room1
+            }
+          }
+        },
+        {
+          fields: {
+            file: {
+              url: room1
+            }
+          }
+        }
+      ]
+    }
+  },
+  {
+    sys: {
+      id: "5"
+    },
+    fields: {
+      name: "single economy",
+      slug: "single-economy",
+      type: "single",
+      price: 100,
+      size: 200,
+      capacity: 1,
+      pets: false,
+      breakfast: false,
+      featured: false,
+      description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed laboriosam praesentium incidunt voluptatum voluptas maxime soluta rerum vitae amet quasi, ipsum cupiditate voluptatibus perferendis corrupti ullam facilis sit aliquid porro.",
+      extras: [
+        "Lorem ipsum dolor sit amet consectetur",
+        "Nam fermentum magna ac tortor auctor, semper laoreet t",
+        "Ut fringilla placerat sapien sed rhoncu",
+        "Vestibulum eu enim vel dolor ornare semper quis ornare ",
+        "Aliquam pharetra vulputate laoreet",
+        "Nulla sit amet tempor libero",
+        "Sed condimentum in odio in congue"
+      ],
+      images: [
+        {
+          fields: {
+            file: {
+              url: img1
+            }
+          }
+        },
+        {
+          fields: {
+            file: {
+              url: room1
+            }
+          }
+        },
+        {
+          fields: {
+            file: {
+              url: room1
+            }
+          }
+        },
+        {
+          fields: {
+            file: {
+              url: room1
+            }
+          }
+        }
+      ]
+    }
+  },
+  {
+    sys: {
+      id: "6"
+    },
+    fields: {
+      name: "single economy",
+      slug: "single-economy",
+      type: "single",
+      price: 100,
+      size: 200,
+      capacity: 1,
+      pets: false,
+      breakfast: false,
+      featured: false,
+      description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed laboriosam praesentium incidunt voluptatum voluptas maxime soluta rerum vitae amet quasi, ipsum cupiditate voluptatibus perferendis corrupti ullam facilis sit aliquid porro.",
+      extras: [
+        "Lorem ipsum dolor sit amet consectetur",
+        "Nam fermentum magna ac tortor auctor, semper laoreet t",
+        "Ut fringilla placerat sapien sed rhoncu",
+        "Vestibulum eu enim vel dolor ornare semper quis ornare ",
+        "Aliquam pharetra vulputate laoreet",
+        "Nulla sit amet tempor libero",
+        "Sed condimentum in odio in congue"
+      ],
+      images: [
+        {
+          fields: {
+            file: {
+              url: img1
+            }
+          }
+        },
+        {
+          fields: {
+            file: {
+              url: room1
+            }
+          }
+        },
+        {
+          fields: {
+            file: {
+              url: room1
+            }
+          }
+        },
+        {
+          fields: {
+            file: {
+              url: room1
+            }
+          }
+        }
+      ]
+    }
+  },
+  {
+    sys: {
+      id: "7"
+    },
+    fields: {
+      name: "single economy",
+      slug: "single-economy",
+      type: "single",
+      price: 100,
+      size: 200,
+      capacity: 1,
+      pets: false,
+      breakfast: false,
+      featured: false,
+      description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed laboriosam praesentium incidunt voluptatum voluptas maxime soluta rerum vitae amet quasi, ipsum cupiditate voluptatibus perferendis corrupti ullam facilis sit aliquid porro.",
+      extras: [
+        "Lorem ipsum dolor sit amet consectetur",
+        "Nam fermentum magna ac tortor auctor, semper laoreet t",
+        "Ut fringilla placerat sapien sed rhoncu",
+        "Vestibulum eu enim vel dolor ornare semper quis ornare ",
+        "Aliquam pharetra vulputate laoreet",
+        "Nulla sit amet tempor libero",
+        "Sed condimentum in odio in congue"
+      ],
+      images: [
+        {
+          fields: {
+            file: {
+              url: img1
+            }
+          }
+        },
+        {
+          fields: {
+            file: {
+              url: room1
+            }
+          }
+        },
+        {
+          fields: {
+            file: {
+              url: room1
+            }
+          }
+        },
+        {
+          fields: {
+            file: {
+              url: room1
+            }
+          }
+        }
+      ]
+    }
+  },
+  {
+    sys: {
+      id: "8"
+    },
+    fields: {
+      name: "single economy",
+      slug: "single-economy",
+      type: "single",
+      price: 100,
+      size: 200,
+      capacity: 1,
+      pets: false,
+      breakfast: false,
+      featured: false,
+      description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed laboriosam praesentium incidunt voluptatum voluptas maxime soluta rerum vitae amet quasi, ipsum cupiditate voluptatibus perferendis corrupti ullam facilis sit aliquid porro.",
+      extras: [
+        "Lorem ipsum dolor sit amet consectetur",
+        "Nam fermentum magna ac tortor auctor, semper laoreet t",
+        "Ut fringilla placerat sapien sed rhoncu",
+        "Vestibulum eu enim vel dolor ornare semper quis ornare ",
+        "Aliquam pharetra vulputate laoreet",
+        "Nulla sit amet tempor libero",
+        "Sed condimentum in odio in congue"
+      ],
+      images: [
+        {
+          fields: {
+            file: {
+              url: img1
+            }
+          }
+        },
+        {
+          fields: {
+            file: {
+              url: room1
+            }
+          }
+        },
+        {
+          fields: {
+            file: {
+              url: room1
+            }
+          }
+        },
+        {
+          fields: {
+            file: {
+              url: room1
+            }
+          }
+        }
+      ]
+    }
+  },
+  {
+    sys: {
+      id: "9"
+    },
+    fields: {
+      name: "single economy",
+      slug: "single-economy",
+      type: "single",
+      price: 100,
+      size: 200,
+      capacity: 1,
+      pets: false,
+      breakfast: false,
+      featured: false,
+      description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed laboriosam praesentium incidunt voluptatum voluptas maxime soluta rerum vitae amet quasi, ipsum cupiditate voluptatibus perferendis corrupti ullam facilis sit aliquid porro.",
+      extras: [
+        "Lorem ipsum dolor sit amet consectetur",
+        "Nam fermentum magna ac tortor auctor, semper laoreet t",
+        "Ut fringilla placerat sapien sed rhoncu",
+        "Vestibulum eu enim vel dolor ornare semper quis ornare ",
+        "Aliquam pharetra vulputate laoreet",
+        "Nulla sit amet tempor libero",
+        "Sed condimentum in odio in congue"
+      ],
+      images: [
+        {
+          fields: {
+            file: {
+              url: img1
+            }
+          }
+        },
+        {
+          fields: {
+            file: {
+              url: room1
+            }
+          }
+        },
+        {
+          fields: {
+            file: {
+              url: room1
+            }
+          }
+        },
+        {
+          fields: {
+            file: {
+              url: room1
+            }
+          }
+        }
+      ]
+    }
+  },
+  {
+    sys: {
+      id: "10"
+    },
+    fields: {
+      name: "single economy",
+      slug: "single-economy",
+      type: "single",
+      price: 100,
+      size: 200,
+      capacity: 1,
+      pets: false,
+      breakfast: false,
+      featured: false,
+      description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed laboriosam praesentium incidunt voluptatum voluptas maxime soluta rerum vitae amet quasi, ipsum cupiditate voluptatibus perferendis corrupti ullam facilis sit aliquid porro.",
+      extras: [
+        "Lorem ipsum dolor sit amet consectetur",
+        "Nam fermentum magna ac tortor auctor, semper laoreet t",
+        "Ut fringilla placerat sapien sed rhoncu",
+        "Vestibulum eu enim vel dolor ornare semper quis ornare ",
+        "Aliquam pharetra vulputate laoreet",
+        "Nulla sit amet tempor libero",
+        "Sed condimentum in odio in congue"
+      ],
+      images: [
+        {
+          fields: {
+            file: {
+              url: img1
+            }
+          }
+        },
+        {
+          fields: {
+            file: {
+              url: room1
+            }
+          }
+        },
+        {
+          fields: {
+            file: {
+              url: room1
+            }
+          }
+        },
+        {
+          fields: {
+            file: {
+              url: room1
+            }
+          }
+        }
+      ]
+    }
+  },
+  {
+    sys: {
+      id: "11"
+    },
+    fields: {
+      name: "single economy",
+      slug: "single-economy",
+      type: "single",
+      price: 100,
+      size: 200,
+      capacity: 1,
+      pets: false,
+      breakfast: false,
+      featured: false,
+      description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed laboriosam praesentium incidunt voluptatum voluptas maxime soluta rerum vitae amet quasi, ipsum cupiditate voluptatibus perferendis corrupti ullam facilis sit aliquid porro.",
+      extras: [
+        "Lorem ipsum dolor sit amet consectetur",
+        "Nam fermentum magna ac tortor auctor, semper laoreet t",
+        "Ut fringilla placerat sapien sed rhoncu",
+        "Vestibulum eu enim vel dolor ornare semper quis ornare ",
+        "Aliquam pharetra vulputate laoreet",
+        "Nulla sit amet tempor libero",
+        "Sed condimentum in odio in congue"
+      ],
+      images: [
+        {
+          fields: {
+            file: {
+              url: img1
+            }
+          }
+        },
+        {
+          fields: {
+            file: {
+              url: room1
+            }
+          }
+        },
+        {
+          fields: {
+            file: {
+              url: room1
+            }
+          }
+        },
+        {
+          fields: {
+            file: {
+              url: room1
+            }
+          }
+        }
+      ]
+    }
+  },
+  {
+    sys: {
+      id: "12"
+    },
+    fields: {
+      name: "single economy",
+      slug: "single-economy",
+      type: "single",
+      price: 100,
+      size: 200,
+      capacity: 1,
+      pets: false,
+      breakfast: false,
+      featured: false,
+      description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed laboriosam praesentium incidunt voluptatum voluptas maxime soluta rerum vitae amet quasi, ipsum cupiditate voluptatibus perferendis corrupti ullam facilis sit aliquid porro.",
+      extras: [
+        "Lorem ipsum dolor sit amet consectetur",
+        "Nam fermentum magna ac tortor auctor, semper laoreet t",
+        "Ut fringilla placerat sapien sed rhoncu",
+        "Vestibulum eu enim vel dolor ornare semper quis ornare ",
+        "Aliquam pharetra vulputate laoreet",
+        "Nulla sit amet tempor libero",
+        "Sed condimentum in odio in congue"
+      ],
+      images: [
+        {
+          fields: {
+            file: {
+              url: img1
+            }
+          }
+        },
+        {
+          fields: {
+            file: {
+              url: room1
+            }
+          }
+        },
+        {
+          fields: {
+            file: {
+              url: room1
+            }
+          }
+        },
+        {
+          fields: {
+            file: {
+              url: room1
+            }
+          }
+        }
+      ]
+    }
+  },
+  {
+    sys: {
+      id: "13"
+    },
+    fields: {
+      name: "single economy",
+      slug: "single-economy",
+      type: "single",
+      price: 100,
+      size: 200,
+      capacity: 1,
+      pets: false,
+      breakfast: false,
+      featured: false,
+      description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed laboriosam praesentium incidunt voluptatum voluptas maxime soluta rerum vitae amet quasi, ipsum cupiditate voluptatibus perferendis corrupti ullam facilis sit aliquid porro.",
+      extras: [
+        "Lorem ipsum dolor sit amet consectetur",
+        "Nam fermentum magna ac tortor auctor, semper laoreet t",
+        "Ut fringilla placerat sapien sed rhoncu",
+        "Vestibulum eu enim vel dolor ornare semper quis ornare ",
+        "Aliquam pharetra vulputate laoreet",
+        "Nulla sit amet tempor libero",
+        "Sed condimentum in odio in congue"
+      ],
+      images: [
+        {
+          fields: {
+            file: {
+              url: img1
+            }
+          }
+        },
+        {
+          fields: {
+            file: {
+              url: room1
+            }
+          }
+        },
+        {
+          fields: {
+            file: {
+              url: room1
+            }
+          }
+        },
+        {
+          fields: {
+            file: {
+              url: room1
+            }
+          }
+        }
+      ]
+    }
+  }
+];
